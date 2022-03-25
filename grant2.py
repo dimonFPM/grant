576310ada23c1b7fc515e420404655d3593fa380
Дмитрий <dimons1s@yandex.ru>
2022-03-25 21:59:32 +0300

    25.3.2022 21:59 grant2

--- grant2.py
+++ grant2.py
@@ -4,10 +4,12 @@ import numpy as np
 import numba as nb
 from loguru import logger
 import warnings
+import os
+import datetime
 
 warnings.filterwarnings("ignore")
 # log1 = logger.add("log.log", level="DEBUG")
-# logger.remove()
+logger.remove()
 logger.debug("старт")
 
 
@@ -39,53 +41,60 @@ def sigma(x: float, xi: float):
 
 
 def finder(x, xi1, xi2):
-    rez = skobka(x, xi2)
+    res = skobka(x, xi2)
     s1 = sigma(x, xi1)
     s2 = sigma(x, xi2)
-    return rez * s1 * s2 * math.cosh(s1) * math.sinh(s2) - x ** 2 * math.sinh(s1) * math.cosh(
+    return res * s1 * s2 * math.cosh(s1) * math.sinh(s2) - x ** 2 * math.sinh(s1) * math.cosh(
         s2)  # пренести в отдельную функцию
 
 
 def main():
-    nu = 0.1
-    xi1 = 0.1
+    name = datetime.datetime.now().strftime('%d-%m-%Y_%H.%M.%S')
+    os.mkdir(f"result_{name}")
+
+    maxX = 10
     shag = 0.01
-    xi2 = f_xi2(nu, xi1)
-    if xi2 == None:
-        logger.debug("error: деление на ноль")
-        exit(500)
-    col = 0
-    col_mnim = 0
-    rez_x = []
-    rez_list = []  # список вещественных значений уравнения
-    for x in np.arange(0, 10 + shag, shag):
-        logger.debug(f"xi2={xi2}")
-        logger.debug(f"x={x}")
-        if (x ** 2 > xi1 and x ** 2 > xi2) or (x ** 2 < xi1 and x ** 2 < xi2):  # *1
-            rez = finder(x, xi1, xi2)
-            rez_x.append(x)
-            rez_list.append(rez)
-            logger.debug(f"x**2={x ** 2}    {rez}\n")
-        else:
-            logger.debug("мнимый корень\n")
-            col_mnim += 1
+    u = (0.1, 0.2, 0.3, 0.4)
+    xi1_list = (0.1, 0.5, 1, 2, 3, 4, 5)
+    for i in xi1_list:
+        xi1 = i
+        for j in u:
+            nu = j
+            xi2 = f_xi2(nu, xi1)
+            if xi2 == None:
+                logger.debug("error: деление на ноль")
+                exit(500)
+            col = 0
+            col_mnim = 0
+            res_x = []
+            res_list = []  # список вещественных значений уравнения
+            for x in np.arange(0, maxX + shag, shag):
+                logger.debug(f"xi2={xi2}")
+                logger.debug(f"x={x}")
+                if (x ** 2 > xi1 and x ** 2 > xi2) or (x ** 2 < xi1 and x ** 2 < xi2):  # *1
+                    rez = finder(x, xi1, xi2)
+                    res_x.append(x)
+                    res_list.append(rez)
+                    logger.debug(f"x**2={x ** 2}    {rez}\n")
+                else:
+                    logger.debug("мнимый корень\n")
+                    col_mnim += 1
 
+            col = 0
+            for i in res_list:
+                if i.imag != 0:
+                    col += 1
+            logger.debug(f"col-vo imag={col}")
+            logger.debug(len(res_list))
+            logger.debug(f"col_mnim={col_mnim}")
 
-
-    col = 0
-    for i in rez_list:
-        if i.imag != 0:
-            col += 1
-    logger.debug(f"col-vo imag={col}")
-    logger.debug(len(rez_list))
-    logger.debug(f"col_mnim={col_mnim}")
-    with open(f"rezalt_nu({nu})_xi1({xi1}).txt", "w") as file:
-        if len(rez_list) == len(rez_x):
-            for i in range(len(rez_list)):
-                if rez_list[i] < 1:
-                    file.write(f"x={'%.2f' % rez_x[i]}    f(u)={'%.5f' % rez_list[i]}\n")
-        else:
-            print("списки разной длины")
+            with open(f"result_{name}/resalt_xi1({xi1})_nu({nu})_shag({shag})_maxX({maxX}).txt", "w") as file:
+                if len(res_list) == len(res_x):
+                    for i in range(len(res_list)):
+                        if res_list[i] < 1:
+                            file.write(f"x={'%.2f' % res_x[i]}    f(u)={'%.5f' % res_list[i]}\n")
+                else:
+                    print("списки разной длины")
 
 
 if __name__ == '__main__':
