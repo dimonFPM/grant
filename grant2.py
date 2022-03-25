0926727f0bf3474d7e5c09575e03ce88af0cfc41
Дмитрий <dimons1s@yandex.ru>
2022-03-25 21:31:03 +0300

    25.3.2022 21:30 grant2

--- grant2.py
+++ grant2.py
@@ -2,47 +2,48 @@ import cmath
 import math
 import numpy as np
 import numba as nb
-
+from loguru import logger
 import warnings
 
 warnings.filterwarnings("ignore")
+# log1 = logger.add("log.log", level="DEBUG")
+# logger.remove()
+logger.debug("старт")
 
 
 @nb.njit(nopython=True, parallel=True)
 def skobka(x: float, xi2: float) -> float or None:
-    # if type(x) == float and type(xi2) == float:
     return (x ** 2 - 0.5 * xi2) ** 2
-    # else:
-    #     return None
 
 
 @nb.njit(nopython=True, parallel=True)
 def f_xi2(nu: float, xi1: float) -> float or None:
-    # if type(nu) == float and type(xi1) == float:
     if nu * 2 != 1:
         return ((2 * (1 - nu)) / (1 - (2 * nu))) * xi1
     else:
-        print("error: деление на ноль")
         return None
-    # else:
-    #     return None
 
 
-@nb.njit(nopython=True, parallel=True)
+# @nb.njit(nopython=True, parallel=True)
 def sigma(x: float, xi: float):
     # if type(x) == float and type(xi) == float:
     if x ** 2 > xi:
-        print("ветка 1")
+        logger.debug("ветка 1")
         return math.sqrt(x ** 2 - xi)
     elif xi > x ** 2:
-        print("ветка 2")
+        logger.debug("ветка 2")
         return math.sqrt(xi - x ** 2)
     else:
-        print("ветка 3")
-        print("Равно")
+        logger.debug("ветка 3")
+        return 0
 
-    # else:
-    #     return None
+
+def finder(x, xi1, xi2):
+    rez = skobka(x, xi2)
+    s1 = sigma(x, xi1)
+    s2 = sigma(x, xi2)
+    return rez * s1 * s2 * math.cosh(s1) * math.sinh(s2) - x ** 2 * math.sinh(s1) * math.cosh(
+        s2)  # пренести в отдельную функцию
 
 
 def main():
@@ -50,53 +51,42 @@ def main():
     xi1 = 0.1
     shag = 0.01
     xi2 = f_xi2(nu, xi1)
+    if xi2 == None:
+        logger.debug("error: деление на ноль")
+        exit(500)
     col = 0
+    col_mnim = 0
+    rez_x = []
+    rez_list = []  # список вещественных значений уравнения
     for x in np.arange(0, 10 + shag, shag):
-        s1 = sigma(x, xi1)
-        s2 = sigma(x, xi2)
-        # rez = skobka(x, xi2)
-        rez = 1
+        logger.debug(f"xi2={xi2}")
+        logger.debug(f"x={x}")
         if (x ** 2 > xi1 and x ** 2 > xi2) or (x ** 2 < xi1 and x ** 2 < xi2):  # *1
-            rez = rez * s1 * s2 * math.cosh(s1) * math.sinh(s2)
-        elif (x ** 2 > xi1 and x ** 2 < xi2) or (x ** 2 < xi1 and x ** 2 > xi2):  # *-1
-            rez = rez * s1 * s2 * math.cosh(s1) * math.sinh(s2) * (-1)
+            rez = finder(x, xi1, xi2)
+            rez_x.append(x)
+            rez_list.append(rez)
+            logger.debug(f"x**2={x ** 2}    {rez}\n")
+        else:
+            logger.debug("мнимый корень\n")
+            col_mnim += 1
 
-        if rez.imag != 0:
+
+
+    col = 0
+    for i in rez_list:
+        if i.imag != 0:
             col += 1
-        print(x)
-        print(f"x={x ** 2}    {rez}\n")
-    print(col)
+    logger.debug(f"col-vo imag={col}")
+    logger.debug(len(rez_list))
+    logger.debug(f"col_mnim={col_mnim}")
+    with open(f"rezalt_nu({nu})_xi1({xi1}).txt", "w") as file:
+        if len(rez_list) == len(rez_x):
+            for i in range(len(rez_list)):
+                if rez_list[i] < 1:
+                    file.write(f"x={'%.2f' % rez_x[i]}    f(u)={'%.5f' % rez_list[i]}\n")
+        else:
+            print("списки разной длины")
 
 
 if __name__ == '__main__':
     main()
-    # # print(skobka(10.0,10.0))
-    # # print(f_xi2(10.0,10.0))
-    # # print(sigma(0.1,5))
-    # nu = 0.1
-    # xi1 = 0.1
-    # shag = 0.01
-    # rez = []
-    # col = 0
-    # for x in np.arange(0, 10 + shag, shag):
-    #     # z = sigma(x, xi1) * sigma(x, f_xi2(nu, xi1)) * cmath.cosh(sigma(x, xi1)) * cmath.sinh(sigma(x, f_xi2(nu, xi1)))
-    #
-    #     # z = sigma(x, f_xi2(nu, xi1)) * cmath.sinh(sigma(x, f_xi2(nu, xi1)))
-    #     z = sigma(x, xi1) * cmath.cosh(sigma(x, xi1))
-    #     # z = cmath.cosh(sigma(x, xi1)) * cmath.sinh(sigma(x, f_xi2(nu, xi1)))
-    #
-    #     # z = sigma(x, xi1)
-    #     # z = sigma(x, f_xi2(nu, xi1))
-    #     # z = cmath.cosh(sigma(x, xi1))
-    #     # z = cmath.sinh(sigma(x, f_xi2(nu, xi1)))
-    #     print(f"xi={f_xi2(nu, xi1)}")
-    #
-    #     if z.imag != 0:
-    #         col += 1
-    #     print(x)
-    #     print(f"x={x ** 2}    {z}\n")
-    #
-    #     rez.append(z)
-    # print(col)
-    # # rez.sort(key=lambda i: i.imag)
-    # # print(*rez, sep="\n")
